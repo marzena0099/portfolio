5539968c4ed807260eba86d938179411f748f9e9
marzenusia <nsnjjs@com.pl>
2025-09-13 13:09:31 +0200

    change on cv from resources

--- src/main/java/org/example/portfolio/controller/CvController.java
+++ src/main/java/org/example/portfolio/controller/CvController.java
@@ -21,7 +21,7 @@ public class CvController {
 
     @GetMapping
     public ResponseEntity<Resource> getCv() {
-        Resource resource = new ClassPathResource("static/Marzena_Białończyk_CV_PL_16.07.2025.pdf");
+        Resource resource = new ClassPathResource("cvs/Marzena_Białończyk_CV_PL_16.07.2025.pdf");
 
         String encodedFileName = URLEncoder.encode("Marzena_Białończyk_CV_PL_16.07.2025.pdf", StandardCharsets.UTF_8);
 
@@ -31,27 +31,3 @@ public class CvController {
                 .body(resource);
     }
 }
-
-
-//    @Transactional(readOnly = true)
-//    public ResponseEntity<byte[]> downloadCv() {
-//        CvFile cv = cvRepository.findTopByOrderByIdDesc();
-//
-//        if(cv == null || cv.getData() == null || cv.getFileName() == null) {
-//            System.err.println("Brak danych CV w bazie!");
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        System.out.println("Pobieram CV: " + cv.getFileName() + ", rozmiar: " + cv.getData().length + " bajtów");
-//
-//
-//        String encodedFileName = URLEncoder.encode(cv.getFileName(), StandardCharsets.UTF_8).replaceAll("\\+", "%20");
-//
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''" + encodedFileName)
-//                .header("Access-Control-Expose-Headers", "Content-Disposition")
-//                .contentType(MediaType.APPLICATION_PDF)
-//                .body(cv.getData());
-//    }
-//
-//}
